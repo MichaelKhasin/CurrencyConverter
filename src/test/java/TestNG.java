8100965478809ab862a12ca61132ad2f994bee40
michaelkhasin <khasin13@gmail.com>
2020-09-04 11:50:33 +0300

    EUR and TestNG classes added

---
+++ src/test/java/TestNG.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class TestNG {
+    @Test
+    public void enteringValueToConvert() {
+        System.out.println("Your conversion results: " + ConversionOptionConversionAmount.getList() + "," + ConversionOptionConversionAmount.getArraylistConversionFlow());
+
+    }
+}
